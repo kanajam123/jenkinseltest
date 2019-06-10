d1281cf9b5e27e4ab6835ea786e5566890af7779
teja407 <tejaatla123@gmail.com>
2019-06-10 12:07:08 +0530

    Update TestRep.java

--- src/jenkinseltest/TestRep.java
+++ src/jenkinseltest/TestRep.java
@@ -19,11 +19,22 @@ public void disp() throws InterruptedException
 	driver.get("https://i3ljoin.itcinfotech.com/owa/");
 	//driver.findElement(By.name("name")).clear();
 	//driver.findElement(By.name("name")).click(); // Keep this click statement even if you are using click before clear.
-	driver.findElement(By.name("username")).sendKeys("itcinfotech\\30143");
+	driver.findElement(By.name("username")).sendKeys("itcinfotech\\30144");
 	driver.findElement(By.name("password")).sendKeys("Tej@485!@#");
 	driver.findElement(By.xpath(".//div[@onclick='clkLgn()'] ")).click();
 	
+	TakesScreenshot scrShot =((TakesScreenshot)driver);
+	File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+	Thread.sleep(300);
+	File DestFile = new File("C:\\Users\\chravitejareddy\\Documents\\images\\image.png");
+	FileUtils.copyFile(SrcFile, DestFile);
+	driver.findElement(By.xpath(".//div[@onclick='clkLgn()'] ")).click();
+	
+	TakesScreenshot scrShot1 =((TakesScreenshot)driver);
+	File SrcFile1=scrShot1.getScreenshotAs(OutputType.FILE);
+	Thread.sleep(300);
+	File DestFile1 = new File("C:\\Users\\chravitejareddy\\Documents\\images\\image1.png");
+	FileUtils.copyFile(SrcFile1, DestFile1);
 
 }
 }
-

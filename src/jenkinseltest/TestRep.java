1dfde6e5c1a2989ecda96cdb6d9607ec4eb22062
teja407 <tejaatla123@gmail.com>
2019-06-10 12:10:21 +0530

    Update TestRep.java

--- src/jenkinseltest/TestRep.java
+++ src/jenkinseltest/TestRep.java
@@ -19,7 +19,7 @@ public void disp() throws InterruptedException
 	driver.get("https://i3ljoin.itcinfotech.com/owa/");
 	//driver.findElement(By.name("name")).clear();
 	//driver.findElement(By.name("name")).click(); // Keep this click statement even if you are using click before clear.
-	driver.findElement(By.name("username")).sendKeys("itcinfotech\\30144");
+	driver.findElement(By.name("username")).sendKeys("itcinfotech\\30143");
 	driver.findElement(By.name("password")).sendKeys("Tej@485!@#");
 	driver.findElement(By.xpath(".//div[@onclick='clkLgn()'] ")).click();
 	
